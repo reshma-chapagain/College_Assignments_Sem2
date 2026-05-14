ce84ca9c9df757bf12c64ccf39a7e346fb8b673b
reshma-chapagain <reshmachapagain13@gmail.com>
2026-05-14 07:33:47 +0545

    Java Array Break Sort and basics

---
+++ Java/src/Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args)
+
+    {
+        double array[];
+        array = new double[10];
+        int intArray [] = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < array.length; i++) {
+            array[i] = intArray[i];
+            System.out.println("double array" + array [i]);
+        }
+
+    }
+}
