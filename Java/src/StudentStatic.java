a32290a061e9f82e9c6ec7e0e3f54552b9164050
reshma-chapagain <reshmachapagain13@gmail.com>
2026-06-04 15:39:48 +0545

    Static Members example

---
+++ Java/src/StudentStatic.java
@@ -0,0 +1,26 @@
+public class StudentStatic {
+
+    int rollno;
+    String name;
+    static String college =" ITS";
+
+
+    StudentStatic(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+
+    void display () {
+        System.out.println("Roll no:" + rollno + " Name:" + name + college);
+    }
+
+    public static void main (String[] args) {
+
+        StudentStatic s1 = new StudentStatic(1,"RAM");
+        StudentStatic s2 = new StudentStatic(2,"Abc");
+
+        s1.display();
+        s2.display();
+    }
+}
